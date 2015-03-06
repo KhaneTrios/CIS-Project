e8ca63ff203de3ec58f1c091cc9408b8f1073226
Chandler <KhaneTrios@outlook.com>
2015-03-05 21:27:52 -0500

    My Rainbow Script
    Fixed the color issues and weird brackets. Uhhhm the whole "variables
    public class" and shit I got rid of because it was causing an error and
    I dont even know if that's how that works. I declared int loop in the
    main class instead.

---
+++ TestScripts/RainbowScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+	//The Main Functions
+public class RainbowScript : MonoBehaviour
+{
+	int loop = 420;
+
+
+	void Update ()
+	{
+		if(Input.GetKeyDown(KeyCode.R))
+		{
+			while (loop < 69)
+			//Double rainbow all the way
+			gameObject.renderer.material.color = Color.white;
+			gameObject.renderer.material.color = Color.red;
+			gameObject.renderer.material.color = Color.yellow;
+			gameObject.renderer.material.color = Color.green;
+			gameObject.renderer.material.color = Color.blue;
+			gameObject.renderer.material.color = Color.magenta;}}}
