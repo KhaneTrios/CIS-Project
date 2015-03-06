d71ec563e40ea00bcd0c233c005dab39ae0e42d6
Tony <Tony_Dougal@hotmail.com>
2015-03-05 22:08:01 -0500

    I hope this works.
    I just want rainbows, man.

---
+++ TestScripts/rainbow4mepls.cs
@@ -0,0 +1,49 @@
+﻿//Chan and Tone made this stuff that'll probably not work.
+
+using UnityEngine;
+using System.Collections;
+
+//The Main Functions
+public class RainbowScript : MonoBehaviour
+{
+	int loop = 420;
+	int timer = 0;
+	
+	void Update ()
+	{
+		if(Input.GetKeyDown(KeyCode.R))
+		{
+			while (loop < 69)
+			{
+				
+				timer = 0;
+				
+				while (timer < 100)
+				{
+					gameObject.renderer.material.color = Color.white;
+					timer = timer + 1;
+				}
+				
+				timer = 0;
+				
+				while (timer < 100)
+				{
+					gameObject.renderer.material.color = Color.red;
+					timer = timer + 1;
+				}
+				
+				timer = 0;
+				
+				while (timer < 100)
+				{
+					gameObject.renderer.material.color = Color.yellow;
+					timer = timer + 1;
+				}
+				
+			//gameObject.renderer.material.color = Color.green;
+			//gameObject.renderer.material.color = Color.blue;
+			//gameObject.renderer.material.color = Color.magenta;
+			}
+		}
+	}
+}
