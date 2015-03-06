a29be355145fb1176125386a837b4f308385321d
Tony <Tony_Dougal@hotmail.com>
2015-03-05 20:52:02 -0500

    Rainbows
    Rainbow Script, Baby.

---
+++ TestScripts/RainbowScript.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+
+//"Global" Variables
+public static class Variables{
+	public static int loop = 420}
+
+//The Main Functions
+public class RainbowScript : MonoBehaviour{
+	void Update(){
+		 if (Input.GetKeyDown(KeyCode.R)){
+			while (loop < 69)
+			//Double rainbow all the way
+			gameObject.renderer.material.color = Color.red;
+			gameObject.renderer.material.color = Color.orange;
+			gameObject.renderer.material.color = Color.yellow;
+			gameObject.renderer.material.color = Color.green;
+			gameObject.renderer.material.color = Color.blue;
+			gameObject.renderer.material.color = Color.purple;}}}
+
