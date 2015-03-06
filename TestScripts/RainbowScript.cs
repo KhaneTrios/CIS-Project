c6b09c2ad99a3646640136b0ca31cacd38f85df8
Tony <Tony_Dougal@hotmail.com>
2015-03-05 21:24:47 -0500

    Bug fixxes
    Bug fixxes

--- TestScripts/RainbowScript.cs
+++ TestScripts/RainbowScript.cs
@@ -7,15 +7,15 @@ public static class Variables{
 	public static int loop = 420}
 
 //The Main Functions
-public class RainbowScript : MonoBehaviour{
+private class RainbowScript : MonoBehaviour{
 	void Update(){
 		 if (Input.GetKeyDown(KeyCode.R)){
-			while (loop < 69)
+			while (loop < 69){
 			//Double rainbow all the way
 			gameObject.renderer.material.color = Color.red;
 			gameObject.renderer.material.color = Color.orange;
 			gameObject.renderer.material.color = Color.yellow;
 			gameObject.renderer.material.color = Color.green;
 			gameObject.renderer.material.color = Color.blue;
-			gameObject.renderer.material.color = Color.purple;}}}
+			gameObject.renderer.material.color = Color.purple;}}}}
 
